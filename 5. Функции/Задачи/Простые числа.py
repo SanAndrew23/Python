853c61901dd570c3fd994c9e32d66de77bbf07fe
Андрей Боев <a.boev.it@gmail.com>
2025-02-08 12:35:09 +0300

    Простые числа (начало)

---
+++ 5. Функции/Задачи/Простые числа.py
@@ -0,0 +1,14 @@
+x = int(input())
+
+count = 0
+number = 2
+while count < x:
+    is_prime = True
+    for i in range(2,  number):
+        if number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(number, end=' ')
+        count += 1
+    number += 1
