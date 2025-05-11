6a7094b0708a1dad08bbf7bdb12e4b5a3c49e7c7
Андрей Боев <a.boev.it@gmail.com>
2025-05-11 10:07:37 +0300

    Небольшие исправления

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-num = abs(int(input()))
-i = 0
-mul = 1
-summa = 0
-while num != 0:
-    if num % 10 % 3 == 0 and num % 10 != 0:
-        summa += (num % 10)
-        mul *= (num % 10)
-    if num % 10 == 0:
-        i += 1
-    num //= 10
-if summa != 0:
-    print(f'{mul / summa:.2f}')
-elif summa != 0 and i > 0:
-    print(f'{mul / summa:.2f}')
-elif summa == 0 and i > 0:
-    print('0')
-else:
-    print('NO')
+s = set()
+def f(x, count):
+    if count == 4:
+        s.add(x)
+        return 1
+    return f(x * 2, count + 1) + f(x / 2, count + 1) + f(x * 3, count +1)
+
+print(f(2560, 0))
+print(s)
+print(len(s))
