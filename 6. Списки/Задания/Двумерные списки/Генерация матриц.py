3f60c1f23d1123e6fe788ba34b801e0f2ef4abcf
AndreyRudenko <rudenkoandrey2304@gmail.com>
2025-06-07 12:44:33 +0300

    Решение задач

---
+++ 6. Списки/Задания/Двумерные списки/Генерация матриц.py
@@ -0,0 +1,32 @@
+def print_matrix(matrix):
+    for row in matrix:
+        for e in row:
+            print(f'{e:3d}', end='')
+        print()
+
+
+def multiplication_table(row, col):
+    return [[i * j for j in range(col)] for i in range(row)]
+
+
+def chess_gen(row, col):
+    return [[(j + i + 1) % 2 for j in range(col)] for i in range(row)]
+
+
+def Andrews_cross(row, col):
+    return [[1 if i == j or j == col - i - 1 else 0 for j in range(col)] for i in range(col)]
+
+
+def cvadratants(row, col):
+    return [[for j in range(col)] for i in range(col)]
+
+
+def vlogennie_squares(row, col):
+    return [[ for j in range(col)] for i in range(col)]
+
+
+def spiral(row, col):
+    return [[ for j in range(col)] for i in range(col)]
+
+
+print_matrix(spiral(7, 7))
--- 6. Списки/Задания/Слияние списков.py
+++ 6. Списки/Задания/Слияние списков.py
@@ -3,17 +3,14 @@ def merge(a, b):
     a_1 = 0
     b_1 = 0
     while a_1 < len(a) and b_1 < len(b):
-        if a[a_1] < b[b_1]:
+        if a[a_1] <= b[b_1]:
             res.append(a[a_1])
             a_1 += 1
         else:
             res.append(b[b_1])
             b_1 += 1
-    if a_1 + b_1 == len(a) + len(b):
-        if a_1 == len(a):
-            res.extend(b)
-        if b_1 == len(b):
-            res.extend(a)
+    res.extend(a[a_1:])
+    res.extend(b[b_1:])
     return res
 
 
