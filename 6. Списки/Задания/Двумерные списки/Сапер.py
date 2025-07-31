3c4b9b817e20147f3ca39fa07dfdb09aa6ffc2af
Андрей Боев <a.boev.it@gmail.com>
2025-07-31 15:13:51 +0300

    Проверка ДЗ (исправление)

--- 6. Списки/Задания/Двумерные списки/Сапер.py
+++ 6. Списки/Задания/Двумерные списки/Сапер.py
@@ -1,4 +1,5 @@
-def create_map(matrix, n, m, i, j):
+def create_map(matrix, i, j):
+    n, m = len(matrix), len(matrix[0])
     matrix[i][j] = '*'
     if i + 1 <= n - 1:
         matrix[i + 1][j] += 1
@@ -16,14 +17,10 @@ def create_map(matrix, n, m, i, j):
         matrix[i + 1][j - 1] += 1
     if j + 1 <= m - 1 and i + 1 <= n - 1:
         matrix[i + 1][j + 1] += 1
-        return matrix
 
-def make_points(matrix, n, m):
-    lst = ['*', '1', '2', '3', '4', '5', '6', '7', '8']
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] not in lst:
-                matrix[i][j] = '.'
+
+def print_matrix(matrix):
+    pass
 
 
 n, m, k = map(int, input().split())
@@ -32,8 +29,6 @@ matrix = [[0] * n for i in range(m)]
 for x in range(k):
     i, j = map(int, input().split())
     i, j = i - 1, j - 1
-    create_map(matrix, n, m, i, j)
+    create_map(matrix, i, j)
 
-make_points(matrix, n, m)
-
-print(matrix)
+print_matrix(matrix)
