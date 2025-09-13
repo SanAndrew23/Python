e6d326f7758af947f2c0856cadb8bea87512e9a6
Андрей Боев <a.boev.it@gmail.com>
2025-09-13 15:09:04 +0300

    Проверка и исправление ДЗ

--- 6. Списки/Задания/Двумерные списки/Генерация матриц.py
+++ 6. Списки/Задания/Двумерные списки/Генерация матриц.py
@@ -30,23 +30,48 @@ def nested_squares(row, col):
 def spiral(row, col):
     matrix = [[0 for j in range(col)] for i in range(row)]
     x = 0
-    for i in range(min(col + 1, row + 1) // 2):
-        for a in range(i, col - i - 1):
+    for i in range(min(row, col) // 2 + (min(row, col) % 2)):
+        for a in range(i, col - i):
             matrix[i][a] = x
             x += 1
 
-        for b in range(i, row - i):
+        for b in range(i + 1, row - i):
             matrix[b][col - 1 - i] = x
             x += 1
 
-        for c in range(row - i, i, -1):
-            matrix[row - i - 1][c] = x
+        if x < row * col:
+            for c in range(col - i - 1, i, -1):
+                matrix[row - i - 1][c - 1] = x
+                x += 1
+        if x < row * col:
+            for d in range(row - i - 1, i + 1, -1):
+                matrix[d - 1][i] = x
+                x += 1
+    '''top, bottom, left, right = 0, row, 0, col
+    while top < bottom and left < right:
+        # Заполнение верхней строки слева направо
+        for i in range(left, right):
+            matrix[top][i] = x
             x += 1
-
-        for d in range(i, row - i - 1):
-            matrix[row - 1 - d][i] = x
+        top += 1
+        # Заполнение правого столбца сверху вниз
+        for i in range(top, bottom):
+            matrix[i][right - 1] = x
             x += 1
+        right -= 1
+        if top < bottom:
+            # Заполнение нижней строки справа налево
+            for i in range(right - 1, left - 1, -1):
+                matrix[bottom - 1][i] = x
+                x += 1
+            bottom -= 1
+        if left < right:
+            # Заполнение левого столбца снизу вверх
+            for i in range(bottom - 1, top - 1, -1):
+                matrix[i][left] = x
+                x += 1
+            left += 1'''
     return matrix
 
 
-print_matrix(spiral(17, 19))
+print_matrix(spiral(2, 7))
---
+++ 7. Множества/main.py
@@ -0,0 +1,14 @@
+nums1 = {1, 2, 3, 4, 5, 6}
+nums2 = {5, 6, 7, 8, 9}
+
+print(nums1.union(nums2))
+print(nums1 | nums2)
+
+print(nums1.intersection(nums2))
+print(nums1 & nums2)
+
+print(nums1.difference(nums2))
+print(nums1 - nums2)
+
+print(nums1.symmetric_difference(nums2))
+print(nums1 ^ nums2)
